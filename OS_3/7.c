0059274e4d6b983ed01646ed4969b9071b8985bb
beaku <iit2017020@iiita.ac.in>
2018-09-04 19:15:05 +0530

    Added 7th

--- OS_3/7.c
+++ OS_3/7.c
@@ -2,13 +2,19 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 #include<stdlib.h>
+#include<fcntl.h>
+
+char buf[100];
 
 void main(void)
 {
     int i;
+    int fd = open("Process Managment.txt",O_RDWR|O_CREAT);
     for(i=1;i<10;i++)
     {
         fork();
-        printf("The process with the PID=%d\n",getpid());
+        sprintf(buf,"The process with the PID=%d\n",getpid());
+        write(fd,buf,strlen(buf));
     }
+    close(fd);
 }
